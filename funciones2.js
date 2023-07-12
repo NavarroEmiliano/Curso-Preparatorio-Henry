0d984c734952e2f9ed5abf938bf0cd7bebd84fd4
Emiliano Navarro <emilianonavarro1993@gmail.com>
2023-07-12 00:09:05 -0300

    Funciones 2

---
+++ funciones2.js
@@ -0,0 +1,16 @@
+function cuidadoConElConsoleLog(nombre) {
+  console.log(nombre);
+  return nombre;
+}
+
+function otraFuncion() {
+  return (
+    "El nombre retornado por la funcion 'cuidadoConElConsoleLog' es: " +
+    cuidadoConElConsoleLog("Emiliano")
+  );
+}
+
+function cuidadoConElReturn(nombre) {
+    return nombre;
+    console.log(nombre)
+}

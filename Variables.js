7d19d073cb6fed40fd458c61bcdb2d2ec76eec6d
Emiliano Navarro <emilianonavarro1993@gmail.com>
2023-07-11 23:20:35 -0300

    Operadores

---
+++ Variables.js
@@ -0,0 +1,23 @@
+// String
+
+var bootcamp = "Henry";
+
+//Number
+
+var numeroEntero = 17;
+var numeroNegatito = -4;
+var numeroDecimal = 11.75;
+
+//Booleanos
+
+var estoyEntendiendo = true;
+
+var estoyAburrido = false;
+
+//Undefined
+
+var cajon1;
+
+//Null
+
+var sinNada = null;
